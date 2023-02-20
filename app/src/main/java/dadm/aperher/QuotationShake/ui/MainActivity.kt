e764939b7724cc0fd88421bf57fcb8c90c21589e
Alberto Pérez Herrero <aperher@upv.edu.es>
2023-02-20 16:00:46 +0100

    Initial commit

---
+++ app/src/main/java/dadm/aperher/QuotationShake/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package dadm.aperher.QuotationShake.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dadm.aperher.QuotationShake.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
