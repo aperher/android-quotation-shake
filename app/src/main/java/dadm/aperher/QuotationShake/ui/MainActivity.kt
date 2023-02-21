c3202b8b7b44d87101831edd5062c7c24bcefedd
Alberto Pérez Herrero <aperher@upv.edu.es>
2023-02-21 16:33:36 +0100

    sidebar, add a menuprovider with about dialog

--- app/src/main/java/dadm/aperher/QuotationShake/ui/MainActivity.kt
+++ app/src/main/java/dadm/aperher/QuotationShake/ui/MainActivity.kt
@@ -2,19 +2,44 @@ package dadm.aperher.QuotationShake.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
 import dadm.aperher.QuotationShake.R
 import dadm.aperher.QuotationShake.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider {
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
-        binding.bottomNavigation.setupWithNavController(navController)
+        navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
+        (binding.bottomNavigation as NavigationBarView).setupWithNavController(navController)
+
+        setSupportActionBar(binding.materialToolbar)
+        val appBarConf : AppBarConfiguration = AppBarConfiguration(setOf(R.id.newQuotationFragment, R.id.favouritesFragment, R.id.settingsFragment))
+        setupActionBarWithNavController(navController, appBarConf)
+
+        addMenuProvider(this)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_about, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        navController.navigate(R.id.aboutDialogFragment)
+        return true
     }
 }
